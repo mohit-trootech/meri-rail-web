e2e752716cf6a697baadf57b9dbf07f4e8ca564b
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-03-03 17:01:13 +0530

    [Update] Temporary Override Authentication

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -47,11 +47,12 @@ const AuthProvider = ({ children }) => {
       });
   };
 
-  // useEffect(() => {
-  //   if (!IGNORE_URL_PATHS.includes(window.location.pathname)) {
-  //     authenticatedUser();
-  //   }
-  // }, []);
+  useEffect(() => {
+    updatePreloader();
+    // if (!IGNORE_URL_PATHS.includes(window.location.pathname)) {
+    //   authenticatedUser();
+    // }
+  }, []);
   const googleAuthLogin = async (data) => {
     id = LoadingToast(LoadingMessages.LOGIN);
     const response = await PostRequest(
