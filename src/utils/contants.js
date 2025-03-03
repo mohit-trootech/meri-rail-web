169f411b699e7271b2582186a8e79ec345930c3b
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-03-03 15:25:09 +0530

    [Update] DNS

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -5,7 +5,8 @@ export const themes = ["light", "dark", "lofi", "black", "night"];
 
 export const IGNORE_URL_PATHS = ["/auth/", "/auth"];
 
-export const BaseUrlPath = "http://65.1.248.240/";
+export const BaseUrlPath =
+  "https://ec2-65-1-248-240.ap-south-1.compute.amazonaws.com/";
 export const urlLogin = "http://127.0.0.1:8000/api/login/";
 export const urlForgotPassword =
   "http://127.0.0.1:8000/accounts/forgot-password/";
